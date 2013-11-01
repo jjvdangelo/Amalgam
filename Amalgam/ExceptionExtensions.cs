47a8b7a1c8bc9b7fa05f1a7810e73b5abc21b216
James D'Angelo <james@thegeekco.com>
2013-10-31 18:16:39 -0700

    Add some helpers.
    Add Maybe, MaybeExtensions, and ExceptionExtensions.

---
+++ Amalgam/ExceptionExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Amalgam
+{
+	using System;
+
+	internal static class ExceptionExtensions
+	{
+		public static T ThrowIfNull<T>(this T value, string paramName)
+			where T : class
+		{
+			if (ReferenceEquals(null, value)) throw new ArgumentNullException(paramName);
+			return value;
+		}
+	}
+}
---
+++ Amalgam/Maybe.cs
@@ -0,0 +1,64 @@
+﻿namespace Amalgam
+{
+	using System;
+	using System.Collections.Generic;
+
+	public struct Maybe<T>
+	{
+		public static readonly Maybe<T> Nothing = new Maybe<T>();
+
+		public readonly bool HasValue;
+		public readonly T Value;
+
+		public override string ToString()
+		{
+			return ToString(string.Empty);
+		}
+
+		public string ToString(string nothing)
+		{
+			return HasValue ? Value.ToString() : nothing;
+		}
+
+		public static implicit operator Maybe<T>(T value)
+		{
+			return new Maybe<T>(value);
+		}
+
+		public Maybe(T value)
+		{
+			Value = value;
+			HasValue = ReferenceEquals(null, value);
+		}
+	}
+
+	public static class MaybeExtensions
+	{
+		public static Maybe<T> ToMaybe<T>(this T value)
+		{
+			return value;
+		}
+
+		public static Maybe<R> Select<T, R>(this T value, Func<T, Maybe<R>> func)
+		{
+			Maybe<T> maybe = value;
+			return maybe.HasValue ? func(value) : Maybe<R>.Nothing;
+		}
+
+		public static Maybe<R> Select<T, R>(this T value, Func<T, R> func)
+		{
+			Maybe<T> maybe = value;
+			return maybe.HasValue ? func(value) : Maybe<R>.Nothing;
+		}
+
+		public static Maybe<R> Select<T, R>(this Maybe<T> value, Func<T, R> func)
+		{
+			return value.HasValue ? func(value.Value) : Maybe<R>.Nothing;
+		}
+
+		public static Maybe<V> SelectMany<T, U, V>(this Maybe<T> maybe, Func<T, Maybe<U>> k, Func<T, U, V> s)
+		{
+			return maybe.SelectMany(x => k(x), s);
+		}
+	}
+}
